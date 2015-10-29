a53d1c81e9f5ac362a4b84007d4ac7dabb6d881c
gulcegunal <gulce_gunal@hotmail.com>
2015-10-29 13:34:53 +0200

    Update message_client.py

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -2,13 +2,14 @@ import socket
 import threading
 Import random
 class readThread (threading.Thread):
-	...
-	...
-	...
+ def __init__(self, socket, message):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.message = message
+    def run(self):
+       
 class writeThread (threading.Thread):
-	...
-	...
-	...
+
 s = socket.socket()
 host ="127.0.0.1"
 port = 12345
