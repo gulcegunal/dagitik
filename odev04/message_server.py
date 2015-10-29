07e853a0c42813d66fa6a9b62a7ea20ee526c217
gulcegunal <gulce_gunal@hotmail.com>
2015-10-29 13:13:28 +0200

    Update message_server.py

--- odev04/message_server.py
+++ odev04/message_server.py
@@ -1,1 +1,35 @@
 
+import threading
+import socket
+
+
+class myThread (threading.Thread):
+	def __init__(self, threadID, clientSocket, clientAddr,message):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.clientSocket = clientSocket
+		self.clientAddr = clientAddr
+		self.message=message
+	def run(self):
+		print "Starting Thread-" + str(self.threadID)
+		data=self.clientSoket.rev(1024)
+		self.setMessage(data)
+		
+		...
+		...
+		print "Ending Thread-" + str(self.threadID)
+		...
+		...
+		...
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host, port))
+s.listen(5)
+while True:
+	print "Waiting for connection"
+	c, addr = s.accept()
+	print 'Got a connection from ', addr
+	threadCounter += 1
+	thread = myThread(threadCounter, c, addr)
+	thread.start()
