c357edfea38cfb3916af29b5e95baf5e31022858
gulcegunal <gulce_gunal@hotmail.com>
2015-10-29 21:18:03 +0200

    Update message_server.py
    degisiklik

--- odev04/message_server.py
+++ odev04/message_server.py
@@ -1,6 +1,8 @@
 
 import threading
 import socket
+import time
+import random
 
 
 class myThread (threading.Thread):
@@ -19,13 +21,33 @@ class myThread (threading.Thread):
                 break
             else:
                 self.clientSocket.send("Got it!")
+        self.clientSocket.close()
         print "Ending receive-thread-" + str(self.threadID)
     def setMessage(self, data):
         self.message = data
     def getMessage(self):
         return self.message
+       
+
+class timeThread(threading.Thread):
+    global message
+    def __init__(self, threadID, clientSocket, myThread):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.clientSocket = clientSocket
+        self.myThread = myThread
+    def run(self):
+        print "Starting time-thread-" + str(self.threadID)
+        while self.myThread.getMessage() != "End":
+            time.sleep(random.randrange(10))
+            currentTime = "Hello, the time is %s" %(time.ctime(time.time()))
+            self.clientSocket.send(currentTime)
+        print "Ending time-thread-" + str(self.threadID)
+        self.clientSocket.close()
 
 
+threadCounter = 0
+message = ""
 s = socket.socket()
 host = socket.gethostname()
 port = 12345
@@ -38,3 +60,5 @@ while True:
 	threadCounter += 1
 	thread = myThread(threadCounter, c, addr)
 	thread.start()
+	tThread = timeThread(threadCounter, c, Thread)
+        tThread.start()
