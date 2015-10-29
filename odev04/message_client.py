8cb90721f1f5b563751ee42480246885de32fe0d
gulcegunal <gulce_gunal@hotmail.com>
2015-10-29 14:28:49 +0200

    Update message_client.py
    degisiklik

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -1,12 +1,21 @@
 import socket
 import threading
-Import random
+import random
 class readThread (threading.Thread):
  def __init__(self, socket, message):
         threading.Thread.__init__(self)
         self.socket = socket
         self.message = message
     def run(self):
+    	
+        while self.message != "End":
+            message = raw_input("")
+            self.setMessage(message)
+            self.socket.send(message)
+    def setMessage(self, data):
+        self.message = data
+    def getMessage(self):
+        return self.message
        
 class writeThread (threading.Thread):
 
