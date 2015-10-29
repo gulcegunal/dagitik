6eed50e1ed4fc86cdf13d9c68f7e9c1893421ea8
gulcegunal <gulce_gunal@hotmail.com>
2015-10-29 19:38:16 +0200

    Update message_client.py
    degisiklik2

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -7,7 +7,7 @@ class readThread (threading.Thread):
         self.socket = socket
         self.message = message
     def run(self):
-    	
+    	print "starting read-thread"
         while self.message != "End":
             message = raw_input("")
             self.setMessage(message)
@@ -18,17 +18,38 @@ class readThread (threading.Thread):
         return self.message
        
 class writeThread (threading.Thread):
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+
+            while True:
+                data = self.socket.recv(1024)
+                if  (data != "End"):
+                    print(data)
 
 s = socket.socket()
+host = socket.gethostname()
+port = 12345
+
+
+s.connect((host,port))
+
 message = ""
 threads = []
-host ="127.0.0.1"
-port = 12345
-s.connect((host, port))
-	...
-	...
-	...
-rThread = readThread(...)
+
+rThread = readThread(s, message)
 rThread.start()
-wThread = writeThread(...)
+wThread = writeThread(s)
 wThread.start()
+
+threads.append(rThread)
+threads.append(wThread)
+
+for t in threads:
+    t.join()
+
+print "Exiting Main Thread"
+s.close()
+
