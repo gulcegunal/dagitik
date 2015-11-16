d0f73c74738590518f49e5a23993fd436d419f33
gulcegunal <gulce_gunal@hotmail.com>
2015-11-16 18:48:06 +0200

    client2

--- odev05/message_client.py
+++ odev05/message_client.py
@@ -7,3 +7,84 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import Queue
 import time
+
+class ReadThread (threading.Thread):
+	def __init__(self, name, csoc, threadQueue, app):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.csoc = csoc
+		self.nickname = ""
+		self.threadQueue = threadQueue
+		self.app = app
+
+	def incoming_parser(self, data):	
+		global socketClosed	
+		if len(data) == 0:
+			return
+
+		rest = data[4:]
+
+		if data[0:3] == "BYE":
+			msg = "-Server- Bye " + rest
+			self.app.cprint(msg)
+			socketClosed = 1
+			return 1
+		if data[0:3] == "ERL":
+			msg = "-Server- Nick not registered."
+			self.app.cprint(msg)
+			return 0
+		if data[0:3] == "HEL":
+			self.nickname = rest
+			msg = "Registered as <" + self.nickname + ">"
+			self.app.cprint(msg)
+			return 0
+		if data[0:3] == "REJ":
+			msg = "-Server- Nick already in used."
+			self.app.cprint(msg)
+			socketClosed = 1
+			return 1
+
+		if data[0:3] == "MNO":
+			msg = "-Server- User cannot be found."
+			self.app.cprint(msg)
+			return 0
+
+		if data[0:3] == "MSG":
+			index = data.index(":")
+			from_nickname = data[4:index]
+			message = data[index+1:]
+			msg = "<" + from_nickname + ">: " + message
+			self.app.cprint(msg)
+			return 0
+
+		if data[0:3] == "SAY":
+			index = data.index(":")
+			from_nickname = data[4:index]
+			message = data[index+1:]
+			msg = "<" + from_nickname + ">" + data[4+len(self.nickname):] 
+			self.app.cprint(msg)
+			return 0
+
+		if data[0:3] == "SYS":
+			msg = "-Server- " + data[4:]
+			self.app.cprint(msg)
+			return 0
+
+		if data[0:3] == "LSA":
+			splitted = rest.split(":")
+			msg = "-Server- Registered nicks: "
+			for i in splitted:
+				msg += i +","
+			msg = msg[:-1]
+			self.app.cprint(msg)
+			return 0
+
+	def run(self):
+		print("Starting " + self.name)
+		while True:
+			data = self.csoc.recv(1024).decode()
+			queue_message = self.incoming_parser(data)	
+			if (queue_message == 1):
+				self.csoc.close()
+				break
+		print("Exiting " + self.name)
