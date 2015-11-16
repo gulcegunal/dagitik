e6589b15cebf84b61e1f084613b317e60db29700
gulcegunal <gulce_gunal@hotmail.com>
2015-11-17 01:34:25 +0200

    client deg

--- odev05/message_client.py
+++ odev05/message_client.py
@@ -8,6 +8,7 @@ from PyQt4.QtGui import *
 import Queue
 import time
 
+socketClosed = 0
 class ReadThread (threading.Thread):
 	def __init__(self, name, csoc, threadQueue, app):
 		threading.Thread.__init__(self)
@@ -109,3 +110,110 @@ class WriteThread (threading.Thread):
 			if socketClosed == 1:
 				break
 		print("Exiting " + self.name)
+
+class ClientDialog(QDialog):
+	''' An example application for PyQt. Instantiate
+	and call the run method to run. '''
+	def __init__(self, threadQueue):
+		self.threadQueue = threadQueue
+        
+		# create a Qt application --- every PyQt app needs one
+		self.qt_app = QApplication(sys.argv)
+        
+		# Call the parent constructor on the current object
+		QDialog.__init__(self, None)
+
+		# Set up the window
+		self.setWindowTitle('IRC Client')
+		self.setMinimumSize(500, 200)
+        
+		# Add a vertical layout
+		self.vbox = QVBoxLayout()
+		
+		# The sender textbox
+		self.sender = QLineEdit("", self)
+		
+		# The channel region
+		self.channel = QTextBrowser()
+		
+		# The send button
+		self.send_button = QPushButton('&Send')
+
+		# Connect the Go button to its callback
+		self.send_button.clicked.connect(self.outgoing_parser)
+		
+		# Add the controls to the vertical layout
+		self.vbox.addWidget(self.channel)
+		self.vbox.addWidget(self.sender)
+		self.vbox.addWidget(self.send_button)
+		
+		# A very stretchy spacer to force the button to the bottom
+		# self.vbox.addStretch(100)
+
+		# Use the vertical layout for the current window
+		self.setLayout(self.vbox)
+		self.flag = 0
+
+	def cprint(self, data):
+		data = time.ctime() + " " + data 
+		self.channel.append(data)
+
+	def outgoing_parser(self):
+		data = self.sender.text()
+		if len(data) == 0:
+			return
+		msg = "-Local-: " + data
+		self.cprint(msg)
+		if data[0:5] == "/nick" and self.flag == 0:			
+			nickname = data[6:]
+			self.threadQueue.put("USR " + nickname)
+			self.flag = 1
+			self.sender.clear()
+			return
+
+		if data[0] == "/" and self.flag == 1:
+			command = data[1:5]
+			if command == "list":
+				self.threadQueue.put("LSQ ")
+
+			elif command == "quit":
+				self.threadQueue.put("QUI ")
+
+			elif command == "msg ":
+				index = data[5:].index(" ")
+				to_nickname = data[5:5+index]
+				message = data[index+6:]
+				msg = to_nickname
+				self.threadQueue.put("MSG " + str(to_nickname) + ":" + str(message))
+
+			else:
+				self.cprint("-Local-: Command Error.") 
+
+		else:
+			self.threadQueue.put("SAY " + data)
+		self.sender.clear()
+	def run(self):
+		''' Run the app and show the main form. '''
+		self.show()
+		self.qt_app.exec_()
+
+s = socket.socket()
+host = sys.argv[1]
+port = int(sys.argv[2])
+s.connect((host,port))
+
+sendQueue = Queue.Queue()
+
+app = ClientDialog(sendQueue)
+
+rt = ReadThread("ReadThread", s, sendQueue, app)
+rt.start()
+
+wt = WriteThread("WriteThread", s, sendQueue)
+wt.start()
+
+app.run()
+
+rt.join() 
+wt.join()
+s.close()
